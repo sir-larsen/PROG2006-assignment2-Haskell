fac8fab16be87011f0ee38f4b160e9b98e31477d
Alexander <alexael@ntnu.no>
2021-05-04 21:35:01 +0200

    Added parsing of int, float and bool literals

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -51,7 +51,7 @@ toWords x = words x
 
 
 makeTstring :: [String] -> (StackElem, [String])
-makeString s = do
+makeTstring s = do
     let stop = elemIndex "\"" s
     let str  = [s !! i | i <- [0..length s], i < fromJust stop]
     let rstr = drop (length str + 1) s
@@ -59,7 +59,43 @@ makeString s = do
         True  -> (Ttypes (Tstring $ unwords str), rstr)
         False -> error "Thou hath encountered syntax error. No closing symbol for string jest"
         
-makeTint :: String -> StackElem
+makeTint :: String -> Maybe StackElem
+makeTint s
+    | isInt s == True = Just (Ttypes (Tint (read s :: Int)))
+    | otherwise = Nothing
+
+makeTfloat :: String -> Maybe StackElem
+makeTfloat s
+    | isFloat s == True = Just (Ttypes (Tfloat (read s :: Float)))
+    | otherwise = Nothing
+
+makeTbool :: String -> Maybe StackElem
+makeTbool s
+    | isBool s == True = Just (Ttypes (Tbool (read s :: Bool)))
+    | otherwise = Nothing
+
+isBool :: String -> Bool
+isBool x
+    | x == "True" = True
+    | x == "False" = True
+    | otherwise = False
+
+isInt :: String -> Bool
+isInt ""  = False
+isInt "." = False
+isInt xs =
+    case dropWhile isDigit xs of
+        "" -> True
+        _  -> False
+
+isFloat :: String -> Bool
+isFloat ""  = False
+isFloat "." = False
+isFloat xs  =
+    case dropWhile isDigit xs of
+        "" -> True
+        ('.':ys) -> all isDigit ys
+        _ -> False
 
 tokenCheck :: [String] -> IO ()
 tokenCheck (x:xs) = do
